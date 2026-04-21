4b3a4923d8aabfbe53d1c8818e4b20583089b2a9
TheIrregularity <63055396+TheIrregularity@users.noreply.github.com>
2026-04-21 23:40:30 +0200

    Added answer highlighting

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-(function() {
+(function () {
+    const tagList = [];
+    const questions = document.querySelectorAll('.trescE');
+    let ansList = []
+    questions.forEach((q, index) => {
+        const nr = index + 1;
+        const row = [
+            q,
+            document.getElementById('odpa' + nr),
+            document.getElementById('odpb' + nr),
+            document.getElementById('odpc' + nr),
+            document.getElementById('odpd' + nr)
+        ];
+        tagList.push(row);
+    });
     let originalForm = document.getElementById('formegzamin');
     let formClone = originalForm.cloneNode(true);
     formClone.style.display = 'none';
@@ -13,24 +27,28 @@
             'Accept': 'text/html'
         }
     })
-    .then(response => response.text())
-    .then(responseText => {
-        let parser = new DOMParser();
-        let doc = parser.parseFromString(responseText, 'text/html');
-        let correctAnswers = doc.querySelectorAll('.odpgood');
-        let answerText = "Correct Answers:\n";
-        window.answerList = [];
-        
-        correctAnswers.forEach(answer => {
-            let questionNumber = answer.id.replace(/\D/g, '');
-            let correctAnswer = answer.querySelector('strong').textContent.trim();
-            answerText += `Question ${questionNumber}: ${correctAnswer}\n`;
-            window.answerList.push(correctAnswer);
-        });
+        .then(response => response.text())
+        .then(responseText => {
+            let parser = new DOMParser();
+            let doc = parser.parseFromString(responseText, 'text/html');
+            let correctAnswers = doc.querySelectorAll('.odpgood');
+            let answerText = "Correct Answers:\n";
+            window.answerList = [];
 
-        console.log(answerText);
-    })
-    .catch(error => {
-        console.error('Error during form submission:', error);
-    });
+            correctAnswers.forEach(answer => {
+                let questionNumber = answer.id.replace(/\D/g, '');
+                let correctAnswer = answer.querySelector('strong').textContent.trim();
+                answerText += `Question ${questionNumber}: ${correctAnswer}\n`;
+                window.answerList.push(correctAnswer);
+                ansList.push(correctAnswer.charAt(0))
+            });
+            for (x = 0; x < correctAnswers.length; x++) {
+                tagList[x][ansList[x].toLowerCase().charCodeAt(0) - 96].style["color"] = "plum"
+            }
+
+            console.log(answerText);
+        })
+        .catch(error => {
+            console.error('Error during form submission:', error);
+        });
 })();
